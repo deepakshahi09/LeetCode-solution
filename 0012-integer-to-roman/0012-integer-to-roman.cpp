829bb3309298ccf4bf6a26531617fa25c4f81ec5
Deepak kumar <147906161+deepakshahi09@users.noreply.github.com>
2024-12-24 18:21:36 +0530

    Time: 8 ms (24.35%), Space: 11.9 MB (55.33%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.cpp
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     string intToRoman(int num) {
-       static vector<int> val{1000,900,500,400,100,90,50,40,10,9,5,4,1};
-        static vector<string> roman{"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+        vector<int> val{1000,900,500,400,100,90,50,40,10,9,5,4,1};
+ vector<string> roman{"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
         
         string ans ="";
         
