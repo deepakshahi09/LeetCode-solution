2a26e22859710bec1ae4673e8e19e3e8c42a8bdc
Deepak kumar <147906161+deepakshahi09@users.noreply.github.com>
2025-06-12 19:11:01 +0530

    Time: 0 ms (100%), Space: 97.4 MB (26.45%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
-    int maxProfit(vector<int>& prices) {
-        int minp = INT_MAX;
+    int maxProfit(vector<int>& p) {
+        int minprice = p[0];
         int maxpro = 0;
-        for(int i=0;i<prices.size();i++){
-            if(prices[i]<minp){
-                minp = prices[i];
+        for(int i=1;i<p.size();i++){
+            if(p[i]<minprice){
+                minprice = p[i];
             }
-            int profit = prices[i]-minp;
-            if(profit > maxpro){
-                maxpro = profit;
+            else{
+                maxpro = max(maxpro,p[i]-minprice);
             }
         }
         return maxpro;
