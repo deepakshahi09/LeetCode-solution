2f85f6916129eee993331f1a3770b91d6365e4f2
Deepak kumar <147906161+deepakshahi09@users.noreply.github.com>
2025-06-18 09:18:45 +0530

    Time: 19 ms (60.4%), Space: 26.3 MB (39.07%) - LeetHub

--- 0941-valid-mountain-array/0941-valid-mountain-array.cpp
+++ 0941-valid-mountain-array/0941-valid-mountain-array.cpp
@@ -2,18 +2,23 @@ class Solution {
 public:
     bool validMountainArray(vector<int>& arr) {
         int n = arr.size();
-        if(n<3){
-            return false;
-        }
+        if (n < 3) return false;
+
         int i = 0;
-        int j = n-1;
-        while(i+1 < n && arr[i] < arr[i+1]){
+
+        // Walk up
+        while (i + 1 < n && arr[i] < arr[i + 1]) {
             i++;
         }
-        while(j> 0 && arr[j-1] > arr[j]){
-            j--;
+
+        // Peak can't be first or last
+        if (i == 0 || i == n - 1) return false;
+
+        // Walk down
+        while (i + 1 < n && arr[i] > arr[i + 1]) {
+            i++;
         }
-        return i>0 && j<n-1 && i==j;
-        
+
+        return i == n - 1;
     }
 };
