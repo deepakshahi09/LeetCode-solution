c6401102e6007ec34ac3ec0dbf87aea1c1904074
Deepak kumar <147906161+deepakshahi09@users.noreply.github.com>
2025-10-13 12:38:24 +0530

    Time: 10 ms (37.29%), Space: 15.1 MB (61.7%) - LeetHub

--- 0227-basic-calculator-ii/0227-basic-calculator-ii.cpp
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.cpp
@@ -8,10 +8,12 @@ public:
         for (int i = 0; i < s.size(); i++) {
             char c = s[i];
 
+            // Build the current number
             if (isdigit(c)) {
                 num = num * 10 + (c - '0');
             }
 
+            // If operator or end of string
             if ((!isdigit(c) && c != ' ') || i == s.size() - 1) {
                 if (op == '+') {
                     st.push(num);
@@ -22,15 +24,15 @@ public:
                 else if (op == '*') {
                     int top = st.top();
                     st.pop();
-                    st.push(top * num); 
+                    st.push(top * num); // ✅ multiply popped value with current number
                 } 
                 else if (op == '/') {
                     int top = st.top();
                     st.pop();
-                    st.push(top / num);
+                    st.push(top / num); // ✅ divide popped value with current number
                 }
 
-              
+                // Update operator and reset number
                 op = c;
                 num = 0;
             }
