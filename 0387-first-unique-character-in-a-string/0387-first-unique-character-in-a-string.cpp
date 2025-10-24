6ba546a4f0bea87450761b960af54864ec48a155
Deepak kumar <147906161+deepakshahi09@users.noreply.github.com>
2025-10-24 19:37:28 +0530

    Time: 0 ms (100%), Space: 14.8 MB (41.55%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
@@ -1,22 +1,50 @@
+// class Solution {
+// public:
+//     int firstUniqChar(string s) {
+//         vector<int>freq(26,0);
+//         queue<pair<char,int>>q;
+//         for(int i=0;i<s.size();i++){
+//             char c = s[i];
+//             freq[c-'a']++;
+//             q.push({c,i});
+
+//             while(!q.empty() && freq[q.front().first - 'a'] > 1){
+//                 q.pop();
+//             }
+//         }
+//         if(!q.empty()){
+//             return q.front().second;
+//         }
+//         return -1;
+
+        
+//     }
+// };
+
+
+// without using queue;
+
+
 class Solution {
 public:
     int firstUniqChar(string s) {
-        vector<int>freq(26,0);
-        queue<pair<char,int>>q;
-        for(int i=0;i<s.size();i++){
-            char c = s[i];
+         // frequency of each character
+         vector<int>freq(26,0);
+        // Step 1: Count frequency of each character
+        for(char c: s){
             freq[c-'a']++;
-            q.push({c,i});
-
-            while(!q.empty() && freq[q.front().first - 'a'] > 1){
-                q.pop();
-            }
         }
-        if(!q.empty()){
-            return q.front().second;
-        }
-        return -1;
+        
 
+        // Step 2: Find first character with frequency = 1
+       for(int i=0;i<s.size();i++){
+        if(freq[s[i] - 'a'] == 1){
+            return i;
+        }
+       }
+       return -1;
+
+        // Step 3: If no unique char found
         
     }
 };
