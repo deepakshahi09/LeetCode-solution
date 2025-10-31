8925909a63404a161001506c4621d2d73795d0ba
Deepak kumar <147906161+deepakshahi09@users.noreply.github.com>
2025-10-31 21:01:22 +0530

    Time: 18 ms (6.44%), Space: 126.5 MB (33.73%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -10,35 +10,81 @@
  */
 class Solution {
 public:
-    bool isPalindrome(ListNode* head) {
-        ListNode*slow = head;
-        ListNode* fast = head;
-        while(fast->next && fast->next->next){
-            fast = fast->next->next;
-            slow = slow->next;
-        }
-        ListNode* prev = NULL;
-        ListNode* next = NULL;
-        ListNode* curr = slow->next;
+    // bool isPalindrome(ListNode* head) {
+        // ListNode*slow = head;
+        // ListNode* fast = head;
+        // while(fast->next && fast->next->next){
+        //     fast = fast->next->next;
+        //     slow = slow->next;
+        // }
+        // ListNode* prev = NULL;
+        // ListNode* next = NULL;
+        // ListNode* curr = slow->next;
 
-        while(curr){
-            next = curr->next;
-            curr->next = prev;
-            prev = curr;
-            curr = next;
-        }
-        ListNode* right = prev;
-        ListNode* left = head;
+        // while(curr){
+        //     next = curr->next;
+        //     curr->next = prev;
+        //     prev = curr;
+        //     curr = next;
+        // }
+        // ListNode* right = prev;
+        // ListNode* left = head;
 
-        while(right){
-            if(right->val != left->val){
+        // while(right){
+        //     if(right->val != left->val){
+        //         return false;
+        //     }
+        //     right = right->next;
+        //     left = left->next;
+        // }
+        // return true;
+
+        
+
+
+
+
+        // using STACK
+
+         //bool isPalindrome(ListNode* head) {
+        // stack<int>st;
+        // ListNode* temp = head;
+        // while(temp){
+        //     st.push(temp->val);
+        //     temp = temp->next;
+        // }
+        // temp = head;
+        // while(temp){
+        //     int val = st.top();
+        //     st.pop();
+        //     if(val != temp->val){
+        //         return false;
+        //     }
+        //     temp = temp->next;
+        // }
+        // return true;
+
+
+        //      Using Recursion
+
+        ListNode* left;
+        bool helper(ListNode* right){
+            if(right == NULL){
+                return true;
+            }
+            bool ispal = helper(right->next);
+            if(!ispal){
                 return false;
             }
-            right = right->next;
+            if(left->val != right->val){
+                return false;
+            }
             left = left->next;
+            return true;
         }
-        return true;
-
+        bool isPalindrome(ListNode* head) {
+            left = head;
+            return helper(head);
         
         
     }
