f4e3534da48aeb956e1e6b1e17f033237483dc97
Deepak kumar <147906161+deepakshahi09@users.noreply.github.com>
2025-11-01 23:55:52 +0530

    Time: 9 ms (49.31%), Space: 7.8 MB (36.56%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,17 +1,19 @@
 class Solution {
 public:
     int fib(int n) {
-        if(n==0 || n==1){
-            return n;
-        }
-        int a = 0,b = 1, c;
-        for(int i=2;i<=n;i++){
-            c = a+b;
-            a = b;
-            b = c;
+        // if(n==0 || n==1){
+        //     return n;
+        // }
+        // int a = 0,b = 1, c;
+        // for(int i=2;i<=n;i++){
+        //     c = a+b;
+        //     a = b;
+        //     b = c;
            
-        }
-        return b;
+        // }
+        // return b;
+        if(n==0 || n==1) return n;
+        return fib(n-1) + fib(n-2);
 
     }
 };
