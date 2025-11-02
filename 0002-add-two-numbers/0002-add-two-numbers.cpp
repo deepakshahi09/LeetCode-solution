d99736aa9d07dbb24049a79d8f997d8ebc389c32
Deepak kumar <147906161+deepakshahi09@users.noreply.github.com>
2025-11-02 23:07:28 +0530

    Time: 1 ms (45.93%), Space: 77.2 MB (45.03%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -8,31 +8,54 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+// class Solution {
+// public:
+//     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+//         ListNode* dummy = new ListNode(-1);
+//         ListNode* temp = dummy;
+
+//         int carry = 0;
+//         while(l1 || l2 || carry){
+//             int sum = carry;
+//             if(l1){
+//                 sum+=l1->val;
+//                 l1 = l1->next;
+//             }
+//             if(l2){
+//                 sum+=l2->val;
+//                 l2 = l2->next;
+//             }
+//             carry = sum/10;
+//             temp->next = new ListNode(sum % 10);
+//             temp = temp->next;
+//         }
+//         return dummy->next;
+        
+//     }
+// };
 
 class Solution {
 public:
-    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        // dummy node to simplify code
-        ListNode* dummy = new ListNode(0);
-        ListNode* curr = dummy;
-        int carry = 0;
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2, int carry = 0) {
+        // Base case: if both lists are empty and no carry, stop recursion
+        if (!l1 && !l2 && carry == 0)
+            return nullptr;
 
-        while (l1 != nullptr || l2 != nullptr || carry) {
-            int sum = carry;
-            if (l1) {
-                sum += l1->val;
-                l1 = l1->next;
-            }
-            if (l2) {
-                sum += l2->val;
-                l2 = l2->next;
-            }
+        // Calculate current sum
+        int sum = carry;
+        if (l1) sum += l1->val;
+        if (l2) sum += l2->val;
 
-            carry = sum / 10;
-            curr->next = new ListNode(sum % 10);
-            curr = curr->next;
-        }
+        // Create node for current digit
+        ListNode* node = new ListNode(sum % 10);
 
-        return dummy->next; // skip dummy head
+        // Recursive call for next nodes
+        node->next = addTwoNumbers(
+            l1 ? l1->next : nullptr,
+            l2 ? l2->next : nullptr,
+            sum / 10
+        );
+
+        return node;
     }
 };
