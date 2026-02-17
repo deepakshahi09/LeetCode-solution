0f460fd7aeeaa00851d6ed212ed0d1b054494ecb
Deepak kumar <147906161+deepakshahi09@users.noreply.github.com>
2026-02-17 14:44:46 +0530

    Time: 0 ms (100%), Space: 14 MB (88.09%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,17 +1,35 @@
+// class Solution {
+// public:
+//     int lengthOfLIS(vector<int>& nums) {
+//         int n = nums.size();
+//         vector<int>dp(n,1);
+//         int ans = 0;
+//         for(int i=0;i<n;i++){
+//             for(int j=0;j<i;j++){
+//                 if(nums[j] < nums[i]){
+//                     dp[i] = max(dp[i],dp[j]+1);
+//                 }
+//             }
+//             ans=max(dp[i],ans);
+//         }
+//         return ans;
+//     }
+// };
+
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
-        int n = nums.size();
-        vector<int>dp(n,1);
-        int ans = 0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<i;j++){
-                if(nums[j] < nums[i]){
-                    dp[i] = max(dp[i],dp[j]+1);
-                }
-            }
-            ans=max(dp[i],ans);
+        vector<int> temp;
+        
+        for(int num : nums) {
+            auto it = lower_bound(temp.begin(), temp.end(), num);
+            
+            if(it == temp.end())
+                temp.push_back(num);
+            else
+                *it = num;
         }
-        return ans;
+        
+        return temp.size();
     }
 };
