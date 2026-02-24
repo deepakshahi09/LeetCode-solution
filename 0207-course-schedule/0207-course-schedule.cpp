49cf7961e0c4abac53d3c88ca13cc6ce92c6b6bc
Deepak kumar <147906161+deepakshahi09@users.noreply.github.com>
2026-02-24 10:19:43 +0530

    Time: 0 ms (100%), Space: 19.2 MB (81.92%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,38 +1,82 @@
 class Solution {
 public:
-    bool dfs(int node,vector<int>&visit, vector<int>&path, vector<vector<int>>& adj){
-        visit[node] = 1;
-        path[node] = 1;
-        for(int i=0;i<adj[node].size();i++){
-            int nei = adj[node][i];
-            if(path[nei] == 1){
-                return true;
-            }
-            else if(visit[nei] == 0){
-                if(dfs(nei,visit,path,adj)){
-                    return true;
-                }
-            }
-        }
-        path[node] = 0;
-        return false;
-    }
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         vector<vector<int>>adj(numCourses);
+        vector<int>indegree(numCourses,0);
         for(int i=0;i<prerequisites.size();i++){
             int u = prerequisites[i][0];
             int v = prerequisites[i][1];
             adj[v].push_back(u);
+            indegree[u]++;
         }
-        vector<int>visit(numCourses,0);
-        vector<int>path(numCourses,0);
+        queue<int>q;
         for(int i=0;i<numCourses;i++){
-            if(visit[i] == 0){
-                if(dfs(i,visit,path,adj)){
-                    return false;
+            if(indegree[i] == 0){
+                q.push(i);
+            }
+        }
+        int co = 0;
+        while(!q.empty()){
+            int node = q.front();
+            co++;
+            q.pop();
+            for(int n : adj[node]){
+                indegree[n]--;
+                if(indegree[n] == 0){
+                    q.push(n);
                 }
             }
         }
-        return true;
+        return co == numCourses;
+        
     }
 };
+
+
+
+
+
+
+
+//                                 DFS + Recursion Stack Method
+//                                 "Cycle detection using DFS."
+
+
+// class Solution {
+// public:
+//     bool dfs(int node,vector<int>&visit, vector<int>&path, vector<vector<int>>& adj){
+//         visit[node] = 1;
+//         path[node] = 1;
+//         for(int i=0;i<adj[node].size();i++){
+//             int nei = adj[node][i];
+//             if(path[nei] == 1){
+//                 return true;
+//             }
+//             else if(visit[nei] == 0){
+//                 if(dfs(nei,visit,path,adj)){
+//                     return true;
+//                 }
+//             }
+//         }
+//         path[node] = 0;
+//         return false;
+//     }
+//     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+//         vector<vector<int>>adj(numCourses);
+//         for(int i=0;i<prerequisites.size();i++){
+//             int u = prerequisites[i][0];
+//             int v = prerequisites[i][1];
+//             adj[v].push_back(u);
+//         }
+//         vector<int>visit(numCourses,0);
+//         vector<int>path(numCourses,0);
+//         for(int i=0;i<numCourses;i++){
+//             if(visit[i] == 0){
+//                 if(dfs(i,visit,path,adj)){
+//                     return false;
+//                 }
+//             }
+//         }
+//         return true;
+//     }
+// };
