a41193f069858420e7ed6ece859175e7ea2fcde0
Deepak kumar <147906161+deepakshahi09@users.noreply.github.com>
2026-05-07 14:20:02 +0530

    Time: 185 ms (9.49%), Space: 198.6 MB (5.96%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,29 +1,59 @@
-#include <bits/stdc++.h>
-using namespace std;
+// class Solution {
+// public:
+//     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+//         vector<int>ans;
+//         deque<int>dq;
+//         int n = nums.size();
+//         for(int i=0;i<n;i++){
+//             while(!dq.empty() && nums[dq.back()] <= nums[i]){
+//                 dq.pop_back();
+//             }
+//             dq.push_back(i);
+//             if(dq.front() <= i-k){
+//                 dq.pop_front();
+//             }
+//             if(i>=k-1){
+//                 ans.push_back(nums[dq.front()]);
+//             }
+//         }
+//         return ans;
+//     }
+// };
+
+
 
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
         vector<int> ans;
-        deque<int> dq; // stores indices of elements
+        list<int> l;
+        int i = 0, j = 0;
 
-        for (int i = 0; i < nums.size(); i++) {
-            // 1. Remove elements that are out of this window
-            if (!dq.empty() && dq.front() == i - k)
-                dq.pop_front();
-
-            // 2. Remove smaller elements from the back
-            while (!dq.empty() && nums[dq.back()] < nums[i])
-                dq.pop_back();
-
-            // 3. Add current element index
-            dq.push_back(i);
-
-            // 4. Store the max (front of deque) when first window is complete
-            if (i >= k - 1)
-                ans.push_back(nums[dq.front()]);
+        if (k > nums.size()) {
+            ans.push_back(*max_element(nums.begin(), nums.end()));
+            return ans;
         }
 
+        while (j < nums.size()) {
+
+            while (!l.empty() && l.back() < nums[j]) {
+                l.pop_back();
+            }
+            l.push_back(nums[j]);
+
+            if (j - i + 1 < k) {
+                j++;
+            }
+            else { // window size == k
+                ans.push_back(l.front());
+
+                if (l.front() == nums[i]) {
+                    l.pop_front();
+                }
+                i++;
+                j++;
+            }
+        }
         return ans;
     }
 };
