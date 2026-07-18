eb2bcf2bbc1eacaa7e671ecbd47a9d0b5ae0f043
Deepak kumar <147906161+deepakshahi09@users.noreply.github.com>
2026-07-18 09:45:39 +0530

    Time: 15 ms (72.15%), Space: 26.2 MB (37.23%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -3,28 +3,30 @@ public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
-        if(grid[0][0]==1 || grid[n-1][m-1]==1)return -1;
-        vector<vector<int>> dist(n, vector<int>(m, 1e9));
+        if(grid[0][0] == 1 || grid[n-1][m-1] == 1) return -1;
+        vector<vector<int>>dist(n,vector<int>(m,1e9));
         queue<pair<int,pair<int,int>>>q;
         dist[0][0] = 0;
-        q.push({0, {0, 0}});
+        q.push({0,{0,0}});
 
-        while (!q.empty()) {
-            auto it = q.front();
+        while(!q.empty()){
+            int dis = q.front().first;
+            int row = q.front().second.first;
+            int col = q.front().second.second;
             q.pop();
-            int dis = it.first;
-            int row = it.second.first;
-            int col = it.second.second;
-            if(row==n-1 && col==m-1)return dis+1;
-            for (int i = -1; i <= 1; i++) {
-                for (int j = -1; j <= 1; j++) {
-                    int nrow = row + i;
-                    int ncol = col + j;
-                    if(i==0 && j==0)continue;
-                    if (nrow < n && nrow >= 0 && ncol < m && ncol >= 0 &&
-                        grid[nrow][ncol] == 0 && dis + 1 < dist[nrow][ncol]) {
-                        dist[nrow][ncol] = dis + 1;
-                        q.push({dis + 1, {nrow, ncol}});
+
+            if(row == n-1 && col == m-1) return dis+1;
+
+            for(int i=-1;i<=1;i++){
+                for(int j=-1;j<=1;j++){
+                    int nrow = row+i;
+                    int ncol = col+j;
+
+                    if(nrow >=0 && nrow <n && ncol >= 0 && ncol < m && grid[nrow][ncol] == 0){
+                        if(dis + 1 < dist[nrow][ncol]){
+                            dist[nrow][ncol] = dis+1;
+                            q.push({dis+1,{nrow,ncol}});
+                        }
                     }
                 }
             }
