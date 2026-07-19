3fcdb065e31750c3b80323dbcfd4d3666208d109
Deepak kumar <147906161+deepakshahi09@users.noreply.github.com>
2026-07-19 09:08:22 +0530

    Time: 80 ms (69.29%), Space: 44 MB (94.96%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.cpp
+++ 0743-network-delay-time/0743-network-delay-time.cpp
@@ -1,30 +1,68 @@
 class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        //Wellman ford algorithm v-1 time relaxion
+        vector<pair<int,int>>adj[n+1];
+        for(auto &it : times){
+            int u = it[0];
+            int v = it[1];
+            int wt = it[2];
+            adj[u].push_back({v,wt});
+        }
         vector<int>dist(n+1,1e9);
         dist[k] = 0;
-        for(int i=0;i<n-1;i++){
-            bool flag = true;
-            for(auto &it : times){
-                int u = it[0];
-                int v = it[1];
-                int wt = it[2];
-                
-                if(dist[u] != 1e9 && dist[u]+wt < dist[v]){
-                    dist[v] = dist[u]+wt;
-                    flag = false;
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
+        pq.push({0,k});
+        while(!pq.empty()){
+            int dis = pq.top().first;
+            int node = pq.top().second;
+            pq.pop();
+            for(auto it : adj[node]){
+                int adjnode = it.first;
+                int adjwt = it.second;
+                if(dis+adjwt < dist[adjnode]){
+                    dist[adjnode] = dis+adjwt;
+                    pq.push({dist[adjnode],adjnode});
                 }
-
             }
-            if(flag == true) break;
         }
         int ans = 0;
         for(int i=1;i<=n;i++){
-            if(dist[i] == 1e9) return -1;
+            if(dist[i] == 1e9){
+                return -1;
+            }
             ans = max(ans,dist[i]);
         }
         return ans;
+
+
+
+        //Wellman ford algorithm v-1 time relaxion
+        // vector<int>dist(n+1,1e9);
+        // dist[k] = 0;
+        // for(int i=0;i<n-1;i++){
+        //     bool flag = true;
+        //     for(auto &it : times){
+        //         int u = it[0];
+        //         int v = it[1];
+        //         int wt = it[2];
+                
+        //         if(dist[u] != 1e9 && dist[u]+wt < dist[v]){
+        //             dist[v] = dist[u]+wt;
+        //             flag = false;
+        //         }
+
+        //     }
+        //     if(flag == true) break;
+        // }
+        // int ans = 0;
+        // for(int i=1;i<=n;i++){
+        //     if(dist[i] == 1e9) return -1;
+        //     ans = max(ans,dist[i]);
+        // }
+        // return ans;
+
+        
+
         
     }
 };
