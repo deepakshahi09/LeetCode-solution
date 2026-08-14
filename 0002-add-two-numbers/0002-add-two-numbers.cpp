5db10c50b2f88f431b5a2cf72d992795ad91b65d
Deepak kumar <147906161+deepakshahi09@users.noreply.github.com>
2026-08-14 16:21:44 +0530

    Time: 0 ms (100%), Space: 77.1 MB (46.63%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -8,54 +8,28 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-// class Solution {
-// public:
-//     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-//         ListNode* dummy = new ListNode(-1);
-//         ListNode* temp = dummy;
-
-//         int carry = 0;
-//         while(l1 || l2 || carry){
-//             int sum = carry;
-//             if(l1){
-//                 sum+=l1->val;
-//                 l1 = l1->next;
-//             }
-//             if(l2){
-//                 sum+=l2->val;
-//                 l2 = l2->next;
-//             }
-//             carry = sum/10;
-//             temp->next = new ListNode(sum % 10);
-//             temp = temp->next;
-//         }
-//         return dummy->next;
-        
-//     }
-// };
-
 class Solution {
 public:
-    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2, int carry = 0) {
-        // Base case: if both lists are empty and no carry, stop recursion
-        if (!l1 && !l2 && carry == 0)
-            return nullptr;
-
-        // Calculate current sum
-        int sum = carry;
-        if (l1) sum += l1->val;
-        if (l2) sum += l2->val;
-
-        // Create node for current digit
-        ListNode* node = new ListNode(sum % 10);
-
-        // Recursive call for next nodes
-        node->next = addTwoNumbers(
-            l1 ? l1->next : nullptr,
-            l2 ? l2->next : nullptr,
-            sum / 10
-        );
-
-        return node;
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* dummy = new ListNode(-1);
+        ListNode* temp = dummy;
+        int sum = 0;
+        while (l1 || l2) {
+            if (l1) {
+                sum += l1->val;
+                l1 = l1->next;
+            }
+            if (l2) {
+                sum += l2->val;
+                l2 = l2->next;
+            }
+            temp->next = new ListNode(sum % 10);
+            temp = temp->next;
+            sum /= 10;
+        }
+        if (sum > 0) {
+            temp->next = new ListNode(sum);
+        }
+        return dummy->next;
     }
 };
