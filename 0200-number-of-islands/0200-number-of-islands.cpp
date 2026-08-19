aa831670b631d2f63382764cf1c1eb40863a898b
Deepak kumar <147906161+deepakshahi09@users.noreply.github.com>
2026-08-19 21:36:17 +0530

    Time: 25 ms (71.89%), Space: 17.3 MB (54.38%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,41 +1,81 @@
 class Solution {
     private:
-    void bfs(int ro,int co,vector<vector<int>>&visit, vector<vector<char>>&grid){
-        queue<pair<int,int>>q;
-        int delrow[] = {-1,0,1,0};
-        int delcol[] = {0,-1,0,1};
+    void dfs(int row, int col,vector<vector<char>>& grid,vector<vector<int>>& visit){
         int n = grid.size();
         int m = grid[0].size();
-        q.push({ro,co});
-       // visit[ro][co] = 1;
-        while(!q.empty()){
-            int row = q.front().first;
-            int col = q.front().second;
-            q.pop();
-            for(int i=0;i<4;i++){
-                int nrow = row+delrow[i];
-                int ncol = col+delcol[i];
-                if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && grid[nrow][ncol] == '1' && !visit[nrow][ncol]){
-                    visit[nrow][ncol] = 1;
-                    q.push({nrow,ncol});
-                }
+        int delrow[] = {-1,0,1,0};
+        int delcol[] = {0,1,0,-1};
+
+        visit[row][col] = 1;
+        
+        for(int i=0;i<4;i++){
+            int nrow = delrow[i] + row;
+            int ncol = delcol[i]+col;
+
+            if(nrow < n && ncol <m && nrow >=0 && ncol >= 0 && !visit[nrow][ncol] && grid[nrow][ncol] == '1'){
+                dfs(nrow,ncol,grid,visit);
             }
         }
+        
     }
 public:
     int numIslands(vector<vector<char>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
-        int count = 0;
         vector<vector<int>>visit(n,vector<int>(m,0));
+        int c = 0;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(grid[i][j] == '1' && !visit[i][j]){
-                    count++;
-                    bfs(i,j,visit,grid);
+                if(!visit[i][j] && grid[i][j] == '1'){
+                    c++;
+                    dfs(i,j,grid,visit);
                 }
             }
         }
-        return count;
+        return c;
     }
 };
+
+
+
+// class Solution {
+//     private:
+//     void bfs(int ro,int co,vector<vector<int>>&visit, vector<vector<char>>&grid){
+//         queue<pair<int,int>>q;
+//         int delrow[] = {-1,0,1,0};
+//         int delcol[] = {0,-1,0,1};
+//         int n = grid.size();
+//         int m = grid[0].size();
+//         q.push({ro,co});
+//        // visit[ro][co] = 1;
+//         while(!q.empty()){
+//             int row = q.front().first;
+//             int col = q.front().second;
+//             q.pop();
+//             for(int i=0;i<4;i++){
+//                 int nrow = row+delrow[i];
+//                 int ncol = col+delcol[i];
+//                 if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && grid[nrow][ncol] == '1' && !visit[nrow][ncol]){
+//                     visit[nrow][ncol] = 1;
+//                     q.push({nrow,ncol});
+//                 }
+//             }
+//         }
+//     }
+// public:
+//     int numIslands(vector<vector<char>>& grid) {
+//         int n = grid.size();
+//         int m = grid[0].size();
+//         int count = 0;
+//         vector<vector<int>>visit(n,vector<int>(m,0));
+//         for(int i=0;i<n;i++){
+//             for(int j=0;j<m;j++){
+//                 if(grid[i][j] == '1' && !visit[i][j]){
+//                     count++;
+//                     bfs(i,j,visit,grid);
+//                 }
+//             }
+//         }
+//         return count;
+//     }
+// };
